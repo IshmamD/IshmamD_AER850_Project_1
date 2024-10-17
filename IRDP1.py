024debc8e761b4978f2bb4a13742fc0e8b07622a
IshmamD <ishmam2001@outlook.com>
2024-10-16 23:32:13 -0400

    Update IRDP1.py

--- IRDP1.py
+++ IRDP1.py
@@ -63,7 +63,7 @@ Y = df['Step']
 
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42, stratify=Y)
 
-#Scaling
+#Scaling... Update: Scaling ignored since it completely deletes any correlation between the features and the target variable
 """
 my_scaler = StandardScaler()
 my_scaler.fit(X_train)
