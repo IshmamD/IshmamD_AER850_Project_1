d714b448cbc730ecf06c5860eacf851f9a809e29
IshmamD <ishmam2001@outlook.com>
2024-10-17 21:23:52 -0400

    Update IRDP1.py

--- IRDP1.py
+++ IRDP1.py
@@ -189,7 +189,7 @@ disp.plot()
 
 #STEP 6
 
-estimators = [best_model1, best_model2]
+estimators = [('lr', best_model1), ('rf', best_model2)]
 clf = StackingClassifier(estimators=estimators, final_estimator=LogisticRegression(random_state=42),cv=5)
 clf.fit(X_train,y_train)
 ypred4 = clf.predict(X_test)
